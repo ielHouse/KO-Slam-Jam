b97503b4cb403ab02336abb51ed84a93738c02ab
Daniel Brunelle <daniel.brunelle@me.com>
2023-01-12 22:25:01 -0700

    Initial Commit

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paridot
+{
+    public class Player : MonoBehaviour
+    {
+        void Awake()
+        {
+        
+        }
+    }
+}
