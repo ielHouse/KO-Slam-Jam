d67ebd35da30bfb8968acb96c53c0db3275b6b9e
Daniel Brunelle <daniel.brunelle@me.com>
2023-01-12 23:18:03 -0700

    Add input mappings and basic character controller functionality

---
+++ Assets/_Scripts/InputReader.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Paridot
+{
+    [CreateAssetMenu(menuName = "Input Reader")]
+    public class InputReader : ScriptableObject, GameInput.IGameplayActions
+    {
+        private GameInput _gameInput;
+
+        public event Action<Vector2> MoveEvent;
+        public event Action JumpEvent;
+        public event Action JumpCancelledEvent;
+
+        private void OnEnable()
+        {
+            if (_gameInput == null)
+            {
+                _gameInput = new GameInput();
+                
+                _gameInput.Gameplay.SetCallbacks(this);
+                
+                _gameInput.Gameplay.Enable();
+            }
+        }
+
+        private void OnDisable()
+        {
+            _gameInput.Gameplay.Disable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MoveEvent?.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                JumpEvent?.Invoke();
+            }
+
+            if (context.phase == InputActionPhase.Canceled)
+            {
+                JumpCancelledEvent?.Invoke();
+            }
+        }
+    }
+}
--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,14 +1,102 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Paridot
 {
     public class Player : MonoBehaviour
     {
-        void Awake()
-        {
+        [SerializeField] private InputReader _input;
+
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float jumpTimeLimit;
+
+        [SerializeField] private LayerMask groundLayer;
+
+        private Rigidbody _rb;
         
+        private Vector3 _moveDirection;
+        private bool _isJumping;
+        private float _timeJumping;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+            
+            _input.MoveEvent += HandleMove;
+            _input.JumpEvent += HandleJump;
+            _input.JumpCancelledEvent += HandleJumpCancelled;
         }
+
+        private void FixedUpdate()
+        {
+            Move();
+            Jump();
+        }
+
+        private void HandleMove(Vector2 dir)
+        {
+            _moveDirection = new Vector3(dir.x, 0, dir.y);
+        }
+
+        private void HandleJump()
+        {
+            if (!IsGrounded())
+            {
+                return;
+            }
+            _isJumping = true;
+            _timeJumping = 0;
+        }
+
+        private void HandleJumpCancelled()
+        {
+            _isJumping = false;
+        }
+
+        private void Move()
+        {
+            if (_moveDirection == Vector3.zero)
+            {
+                return;
+            }
+
+            transform.position += _moveDirection * (moveSpeed * Time.deltaTime);
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.CheckSphere(transform.position, 1.1f, (int)groundLayer);
+        }
+
+        private void Jump()
+        {
+            if (!_isJumping)
+            {
+                return;
+            }
+
+            if (_timeJumping >= jumpTimeLimit)
+            {
+                _isJumping = false;
+                return;
+            }
+
+            _timeJumping += Time.deltaTime;
+
+            if (IsGrounded())
+            {
+                _rb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            }
+            else
+            {
+                _rb.AddForce(Vector3.up * jumpForce);
+            }
+            
+        }
+
     }
 }
